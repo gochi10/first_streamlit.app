371bfb5b687d516e9a54977f694ee7a7d5c44910
gochi10 <38463253+gochi10@users.noreply.github.com>
2022-05-23 12:50:49 +0800

    Update streamlit_app.py
    added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Chicken Nuggests with Rice')
+streamlit.text('Corn beef, Egg, Rice')
