66a18f376e20355665f5ef16dff1b193284f5560
gochi10 <38463253+gochi10@users.noreply.github.com>
2022-05-25 15:03:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit
 import pandas
 # New Section to display fruityvice api response
 import requests
-streamlit.text(fruityvice_response)
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
